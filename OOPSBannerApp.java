d4f02858c5d769e454bdedc919e0ed3fb5eed2c9
Farid2302 <mm3368@srmist.edu.in>
2026-02-21 21:12:01 +0530

    Create OOPSBannerApp.java

---
+++ OOPSBannerApp.java
@@ -0,0 +1,12 @@
+/**
+ * OOPS Banner App - Use Case 1
+ * Prints the literal text "OOPS" to the console.
+ *
+ * @author Student
+ * @version 1.0
+ */
+public class OOPSBannerApp {
+    public static void main(String[] args) {
+        System.out.println("OOPS");
+    }
+}
