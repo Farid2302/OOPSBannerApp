79a59d1ab47995be890aff32e4231716ac8b8329
Farid2302 <mm3368@srmist.edu.in>
2026-02-21 21:59:06 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,11 +1,4 @@
-/**
- * OOPS Banner App - Use Case 2
- * Displays "OOPS" in a large banner made from spaces and '*' characters
- * using sequential print statements.
- *
- * @author Student
- * @version 1.0
- */
+
 public class OOPSBannerApp {
     public static void main(String[] args) {
         System.out.println("   ***       ***    ******       *****");
