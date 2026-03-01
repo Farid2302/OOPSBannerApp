ffc453e6427c79a76209336194b586bb55e9d15e
Farid2302 <mm3368@srmist.edu.in>
2026-03-01 17:50:33 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,20 @@
-/**
- * OOPS Banner App - Use Case 1
- * Prints the literal text "OOPS" to the console.
- *
- * @author Student
- * @version 1.0
- */
 public class OOPSBannerApp {
+
     public static void main(String[] args) {
-        System.out.println("OOPS");
+
+        String[] lines = {
+            String.join("   ", "  ***  ", "  ***  ", "****** ", " ***** "),
+            String.join("   ", "**   **", "**   **", "**   **", "**     "),
+            String.join("   ", "**   **", "**   **", "**   **", "**     "),
+            String.join("   ", "**   **", "**   **", "****** ", " ***** "),
+            String.join("   ", "**   **", "**   **", "**     ", "     **"),
+            String.join("   ", "**   **", "**   **", "**     ", "     **"),
+            String.join("   ", "**   **", "**   **", "**     ", "     **"),
+            String.join("   ", "  ***  ", "  ***  ", "**     ", " ***** ")
+        };
+
+        for (String line : lines) {
+            System.out.println(line);
+        }
     }
 }
