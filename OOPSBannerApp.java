a0043bbac1887a42ad2a8ed76c9e9a0cd1362e05
Farid2302 <mm3368@srmist.edu.in>
2026-03-10 08:00:40 +0530

    Update OOPSBannerApp.java

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,12 +1,54 @@
-/**
- * OOPS Banner App - Use Case 1
- * Prints the literal text "OOPS" to the console.
- *
- * @author Student
- * @version 1.0
- */
-public class OOPSBannerApp {
+import java.util.HashMap;
+import java.util.Map;
+
+public class BannerUC8 {
     public static void main(String[] args) {
-        System.out.println("OOPS");
+        Map<Character, String[]> patternMap = buildPatternMap();
+        displayBanner("OOPS", patternMap);
+    }
+
+    public static Map<Character, String[]> buildPatternMap() {
+        Map<Character, String[]> map = new HashMap<>();
+        map.put('O', new String[]{
+                "  *****  ",
+                " *     * ",
+                "*       *",
+                "*       *",
+                "*       *",
+                " *     * ",
+                "  *****  "
+        });
+        map.put('P', new String[]{
+                " ******  ",
+                " *     * ",
+                " *     * ",
+                " ******  ",
+                " *       ",
+                " *       ",
+                " *       "
+        });
+        map.put('S', new String[]{
+                "  *****  ",
+                " *     * ",
+                " *       ",
+                "  *****  ",
+                "        *",
+                " *     * ",
+                "  *****  "
+        });
+        return map;
+    }
+
+    public static void displayBanner(String message, Map<Character, String[]> patternMap) {
+        for (int row = 0; row < 7; row++) {
+            StringBuilder sb = new StringBuilder();
+            for (int col = 0; col < message.length(); col++) {
+                char ch = message.charAt(col);
+                if (patternMap.containsKey(ch)) {
+                    sb.append(patternMap.get(ch)[row]).append("   ");
+                }
+            }
+            System.out.println(sb.toString());
+        }
     }
 }
